b214199c716bb95b4189fa12bfcf71eef803f601
John Osterfeld <jczarfeld@gmail.com>
2021-08-22 11:45:03 -0400

    Linked the pages together, added a navbar as well

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import Contact from "./Contact";
 import Portfolio from "./Portfolio";
 import About from "./About";
 import Resume from "./Resume";
+import {Route, Link} from "react-router-dom";
+import Nav from "./nav";
 
 function App() {
-  return <div className="App">
-      <Home />
-      <Portfolio />
-      <About />
-      <Contact />
-      <Resume />
-    </div>;
+  return (
+  <div className="App">
+    <Nav />
+   <Route exact path = "/" component = {Home} />
+   <Route exact path = "/about" component = {About} />
+   <Route exact path = "/portfolio" component = {Portfolio} />
+   <Route exact path = "/contact" component = {Contact} />
+   <Route exact path = "/resume" component = {Resume} />
+    </div>
+  );
 }
 
 
