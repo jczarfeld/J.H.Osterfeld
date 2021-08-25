77b98623a33d6d825b68669107c86142573773b4
John Osterfeld <jczarfeld@gmail.com>
2021-08-25 16:15:56 -0400

    empty data page and styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import { useState } from "react";
 import "./contact.scss";
 
-function Contact() {
-    return (
-        <div className="contact" id="contact">
-        <h1> Contact Me</h1>
-        </div>
-    );
-    
-    
+export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Message Received!</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
-export default Contact;
